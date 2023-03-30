4694ea17663c4ad55457637d6c100164b9114fda
bun beos <trungkienttmh@gmail.com>
2023-03-30 16:04:08 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,30 @@ import Cart from "./pages/Cart/Cart";
 import Collection from "./pages/Collection/Collection";
 import Product from "./pages/Product/Product";
 import NotFound from "./pages/NotFound/NotFound";
-import Body from "./components/Body/Body"
+import Body from "./components/Body/Body";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCGCOiSM9zpb3k_nmv8OnHUbDu-a2jasqE",
+  authDomain: "web-chanh-pharma.firebaseapp.com",
+  projectId: "web-chanh-pharma",
+  storageBucket: "web-chanh-pharma.appspot.com",
+  messagingSenderId: "245445954539",
+  appId: "1:245445954539:web:b39b61032f65624ec6e7ae",
+  measurementId: "G-9KV9WCS9S4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const App = () => {
   return (
     <Router>
